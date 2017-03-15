f0b21efb455e131eebeb0ec406a242a72af31015
Jonathan Meyer <jmeyer@swri.org>
2017-03-14 22:42:56 -0500

    Initial commit of a procedure that works by performing a greedy search along the end-points of a set of path segments.

--- src/noether_path_planner.cpp
+++ src/noether_path_planner.cpp
@@ -5,9 +5,126 @@
 #include <noether/noether.h>
 #include <noether_conversions/noether_conversions.h>
 #include <path_sequence_planner/simple_path_sequence_planner.h>
+#include <eigen_conversions/eigen_msg.h>
 
 namespace
 {
+
+struct PathEndPoints
+{
+  geometry_msgs::Point a;
+  geometry_msgs::Point b;
+};
+
+struct SequencePoint
+{
+  int id;
+  bool from_a;
+};
+
+std::vector<PathEndPoints> toEndPoints(const std::vector<geometry_msgs::PoseArray>& segments)
+{
+  std::vector<PathEndPoints> result;
+  for (const auto& s : segments)
+  {
+    const auto& a = s.poses.front().position;
+    const auto& b = s.poses.back().position;
+    result.push_back({a, b});
+  }
+  return result;
+}
+
+SequencePoint getNextSequencePoint(const geometry_msgs::Point& current, const std::vector<SequencePoint>& so_far,
+                                   const std::vector<PathEndPoints>& path_end_points)
+{
+  SequencePoint min_dist_seq;
+  double min_dist = std::numeric_limits<double>::max();
+
+  auto already_in_sequence = [&so_far] (const int idx) {
+    for (const auto& s : so_far) {
+      if (s.id == idx) return true;
+    }
+    return false;
+  };
+
+  auto pt_dist = [](const geometry_msgs::Point& a, const geometry_msgs::Point& b)
+  {
+    return std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2) + std::pow(a.z - b.z, 2);
+  };
+
+  for (std::size_t i = 0; i < path_end_points.size(); ++i)
+  {
+    if (!already_in_sequence(i))
+    {
+      const auto dist_to_a = pt_dist(current, path_end_points[i].a);
+      const auto dist_to_b = pt_dist(current, path_end_points[i].b);
+
+      if (dist_to_a < min_dist)
+      {
+        min_dist = dist_to_a;
+        min_dist_seq.id = i;
+        min_dist_seq.from_a = true;
+      }
+
+      if (dist_to_b < min_dist)
+      {
+        min_dist = dist_to_b;
+        min_dist_seq.id = i;
+        min_dist_seq.from_a = false;
+      }
+    }
+  }
+  return min_dist_seq;
+}
+
+std::vector<geometry_msgs::PoseArray> makeSequence(const std::vector<geometry_msgs::PoseArray>& in,
+                                                   const std::vector<SequencePoint>& seqs)
+{
+  assert(in.size() == seqs.size());
+  std::vector<geometry_msgs::PoseArray> rs;
+  rs.reserve(in.size());
+
+  for (const auto& seq : seqs)
+  {
+    rs.push_back(in[seq.id]);
+    if (!seq.from_a)
+    {
+      std::reverse(rs.back().poses.begin(), rs.back().poses.end());
+    }
+  }
+  return rs;
+}
+
+std::vector<geometry_msgs::PoseArray> sequence(const std::vector<geometry_msgs::PoseArray>& input)
+{
+  auto end_points = toEndPoints(input);
+
+  auto get_current_pt = [&end_points] (const SequencePoint& p) {
+    if (p.from_a)
+      return end_points[p.id].b;
+    else
+      return end_points[p.id].a;
+  };
+
+  std::vector<SequencePoint> sequence;
+  sequence.reserve(input.size());
+
+  // Always start with the first point in the first sequence
+  sequence.push_back({0, true});
+
+  for (std::size_t i = 1; i < input.size(); ++i)
+  {
+    // Get the current 'point'
+    const auto& pt = get_current_pt(sequence.back());
+
+    // Pick the minimum and add it to the sequence
+    const auto next_sequence_pt = getNextSequencePoint(pt, sequence, end_points);
+    sequence.push_back(next_sequence_pt);
+  }
+
+  assert(sequence.size() == input.size());
+  return makeSequence(input, sequence);
+}
 
 tool_path_planner::ProcessTool loadTool()
 {
@@ -56,13 +173,16 @@ bool godel_noether::NoetherPathPlanner::generatePath(
   auto process_paths = planPaths(vtk_data, tool);
   ROS_INFO("generatePath: finished planning paths");
 
-  //Sequence Paths
-  path_sequence_planner::SimplePathSequencePlanner sequencer;
-  sequencer.setPaths(process_paths);
-  sequencer.linkPaths();
+//  //Sequence Paths
+//  path_sequence_planner::SimplePathSequencePlanner sequencer;
+//  sequencer.setPaths(process_paths);
+//  sequencer.linkPaths();
 
-  // Convert to ROS pose array types
-  path = noether::convertVTKtoGeometryMsgs(sequencer.getPaths());
+//  // Convert to ROS pose array types
+//  path = noether::convertVTKtoGeometryMsgs(sequencer.getPaths());
+  auto paths = noether::convertVTKtoGeometryMsgs(process_paths);
+  path = sequence(paths);
+
   ROS_INFO("generatePath: converted to ROS messages - DONE!");
 
   return true;
